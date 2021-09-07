47ae69e02f750bdae91aaa1d70724df3315885ae
razexay <44016866+razexay@users.noreply.github.com>
2021-09-07 15:48:08 +0300

    Add files via upload

---
+++ yahtzee.js
@@ -0,0 +1,29 @@
+var randomNumber = Math.floor(Math.random() * 6 + 1); // 0-6
+var playerOneImages = "images/dice"+randomNumber+".png";
+
+document.querySelector('.img1').setAttribute('src', playerOneImages);
+
+var randomNumber = Math.floor(Math.random() * 6 + 1); // 0-6
+var playerOneImages = "images/dice"+randomNumber+".png";
+
+document.querySelector('.img2').setAttribute('src', playerOneImages);
+
+var randomNumber = Math.floor(Math.random() * 6 + 1); // 0-6
+var playerOneImages = "images/dice"+randomNumber+".png";
+
+document.querySelector('.img3').setAttribute('src', playerOneImages);
+
+var randomNumber = Math.floor(Math.random() * 6 + 1); // 0-6
+var playerOneImages = "images/dice"+randomNumber+".png";
+
+document.querySelector('.img4').setAttribute('src', playerOneImages);
+
+var randomNumber = Math.floor(Math.random() * 6 + 1); // 0-6
+var playerOneImages = "images/dice"+randomNumber+".png";
+
+document.querySelector('.img5').setAttribute('src', playerOneImages);
+
+var randomNumber = Math.floor(Math.random() * 6 + 1); // 0-6
+var playerOneImages = "images/dice"+randomNumber+".png";
+
+document.querySelector('.img6').setAttribute('src', playerOneImages);
