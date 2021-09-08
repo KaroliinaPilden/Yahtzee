7bc2279e6026547bf56bc6dfc38368c2b7bdf430
KaroliinaPilden <44017827+KaroliinaPilden@users.noreply.github.com>
2021-09-08 11:11:52 +0300

    Update yahtzee.js

--- yahtzee.js
+++ yahtzee.js
@@ -1,38 +1,39 @@
 function roll() {
-var randomNumber = Math.floor(Math.random() * 6 + 1); // 0-6
-var playerOneImages = "images/d"+randomNumber+".png";
+    var randomNumber = Math.floor(Math.random() * 6 + 1); // 0-6
+    var playerOneImages = "images/d"+randomNumber+".png";
 
-document.querySelector('.img1').setAttribute('src', playerOneImages);
+    document.getElementById("img1").setAttribute('src', playerOneImages);
 
-var randomNumber = Math.floor(Math.random() * 6 + 1); // 0-6
-var playerOneImages = "images/d"+randomNumber+".png";
+    var randomNumber = Math.floor(Math.random() * 6 + 1); // 0-6
+    var playerOneImages = "images/d"+randomNumber+".png";
 
-document.querySelector('.img2').setAttribute('src', playerOneImages);
+    document.getElementById("img2").setAttribute('src', playerOneImages);
 
-var randomNumber = Math.floor(Math.random() * 6 + 1); // 0-6
-var playerOneImages = "images/d"+randomNumber+".png";
+    var randomNumber = Math.floor(Math.random() * 6 + 1); // 0-6
+    var playerOneImages = "images/d"+randomNumber+".png";
 
-document.querySelector('.img3').setAttribute('src', playerOneImages);
+    document.getElementById("img3").setAttribute('src', playerOneImages);
 
-var randomNumber = Math.floor(Math.random() * 6 + 1); // 0-6
-var playerOneImages = "images/d"+randomNumber+".png";
+    var randomNumber = Math.floor(Math.random() * 6 + 1); // 0-6
+    var playerOneImages = "images/d"+randomNumber+".png";
 
-document.querySelector('.img4').setAttribute('src', playerOneImages);
+    document.getElementById("img4").setAttribute('src', playerOneImages);
 
-var randomNumber = Math.floor(Math.random() * 6 + 1); // 0-6
-var playerOneImages = "images/d"+randomNumber+".png";
+    var randomNumber = Math.floor(Math.random() * 6 + 1); // 0-6
+    var playerOneImages = "images/d"+randomNumber+".png";
 
-document.querySelector('.img5').setAttribute('src', playerOneImages);
+    document.getElementById("img5").setAttribute('src', playerOneImages);
 }
 
 function score(id) {
-  var x = document.getElementById("score").value;
-  document.getElementById(id).innerHTML = x;
+    var x = document.getElementById("score").value;
+    document.getElementById(id).innerHTML = x;
 }
 
 function sum() {
-  var y = +document.getElementById("ones1").value;
-  var z = +document.getElementById("twos1").value;
-  var u = +y + +z;
-  document.getElementById("sum1").innerHTML = u;
+    var y = +document.getElementById("ones1").innerText;
+    var z = +document.getElementById("twos1").innerText;
+    console.log(y + z)
+    var u = +y + +z;
+    document.getElementById("sum1").innerHTML = u;
 }
